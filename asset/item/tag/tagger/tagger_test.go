b6a734bf5dd46c0e3bead1a697f63bbf2664c2d4
rogelioConsejo <rogelio.consejo@gmail.com>
2022-11-13 17:13:26 +0100

    refactor: test refactor

--- asset/item/tag/tagger/tagger_test.go
+++ asset/item/tag/tagger/tagger_test.go
@@ -3,34 +3,67 @@ package tagger
 import (
 	"errors"
 	"os"
+	"sampo/asset/item"
 	"testing"
 )
 
 func TestTagger(t *testing.T) {
-	t.Parallel()
-	const testQRFileLocation = "./test-qr.png"
-	t.Cleanup(func() {
-		_ = os.Remove(testQRFileLocation)
-	})
-
+	setupTest(t)
 	var testTagger Tagger = New()
-	tag := testTagger.Tag()
-	id := tag.Item().Id
+	id := getIdFromTagger(testTagger)
+	validateIdAsUUID(t, id)
+	err := testTagger.MakeQR(testQRFileLocation)
+	checkIfQRWasCorrectlyCreated(t, err)
+}
 
-	if tagLength := len(id); tagLength != 36 {
-		t.Fatalf("invalid item Id length, expected 10, got %d", tagLength)
-	}
-	if id[8] != '-' || id[13] != '-' || id[18] != '-' {
-		t.Fatalf("invalid format")
-	}
-
-	var qrLocation FilePath = testQRFileLocation
-	err := testTagger.MakeQR(qrLocation)
+func checkIfQRWasCorrectlyCreated(t *testing.T, err error) {
 	if err != nil {
 		t.Fatalf("could not get QR: %s", err.Error())
 	}
-	
-	if _, err := os.Stat(string(qrLocation)); errors.Is(err, os.ErrNotExist) {
+	fileNotFound := checkIfFileIsNotFound(err, testQRFileLocation)
+	if fileNotFound {
 		t.Fatalf("qr file not found")
 	}
 }
+
+func validateIdAsUUID(t *testing.T, id item.Id) {
+	idLength, idLengthIsNotValid := checkIfLengthIsNotValid(id)
+	formatIsNotValid := checkIfFormatIsNotValid(id)
+	if idLengthIsNotValid {
+		t.Fatalf("invalid item Id length, expected %d, got %d", expectedIdLength, idLength)
+	}
+	if formatIsNotValid {
+		t.Fatalf("invalid format")
+	}
+}
+
+func checkIfLengthIsNotValid(id item.Id) (int, bool) {
+	tagLength := len(id)
+	tagLengthIsNotValid := tagLength != expectedIdLength
+	return tagLength, tagLengthIsNotValid
+}
+
+func getIdFromTagger(tagger Tagger) item.Id {
+	tag := tagger.Tag()
+	id := tag.Item().Id
+	return id
+}
+
+func setupTest(t *testing.T) {
+	t.Parallel()
+	t.Cleanup(func() {
+		_ = os.Remove(testQRFileLocation)
+	})
+}
+
+func checkIfFileIsNotFound(err error, qrLocation FilePath) bool {
+	_, err = os.Stat(string(qrLocation))
+	return errors.Is(err, os.ErrNotExist)
+}
+
+func checkIfFormatIsNotValid(id item.Id) bool {
+	return id[8] != '-' || id[13] != '-' || id[18] != '-'
+}
+
+const testQRFileLocation = "./test-qr.png"
+const expectedIdLength = 36
